4f6b777393e6987df24f7be348a8f07c4dee6714
AokoMercyline <aokomercyline34@gmail.com>
2020-09-13 23:58:12 +0300

    imported flask and initialize my app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 # Initializing application
-# app = Flask(__name__,instance_relative_config = True)
+from flask import Flask
+from flask_bootstrap import Bootstrap
 
+app = Flask(__name__,instance_relative_config = True)
+import requests
 # Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile("config.py")
 
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
