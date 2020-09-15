26517f6e3862232a5ba053c0a61d43a235ad22a8
AokoMercyline <aokomercyline34@gmail.com>
2020-09-15 17:57:10 +0300

    modified app struture

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    NEWS_API_BASE_URL ='https://api.themoviedb.org/3/news/{}?api_key={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 class ProdConfig(Config):
     pass
 class DevConfig(Config):
@@ -10,24 +10,3 @@ config_options = {
 'development':DevConfig,
 'production':ProdConfig
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Config:
-#     API_KEY = ''
-    
-# class DevConfig(Config):
-#     pass
-
-# class ProdCOnfig(Config):
-#     pass
